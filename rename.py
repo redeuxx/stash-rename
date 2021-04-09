cc5f2bf45ebf8a9bd3166da68e285f9133cf84fd
Vernon Wenberg <vernon@wenberg.net>
2021-04-09 05:41:23 -0500

    Split file/directory names. Prepend/append to filenames only.

--- rename.py
+++ rename.py
@@ -1,7 +1,6 @@
 import os, sys
 
 options = ("-d", "--prepend", "--append")
-print(options)
 
 if len(sys.argv) < 4:
     sys.exit("Nothing to do.")
@@ -12,10 +11,11 @@ final_name = []
 directory = (sys.argv[(sys.argv.index(options[0])) + 1])
 if not os.path.isdir(directory):
     sys.exit("Specified directory is not a valid directory.")
-filenames_dirs = next(os.walk(directory))[2] # list of files, just filenames, no directories 
+filenames_no_dirs = next(os.walk(directory))[2] # list of files, just filenames, no directories 
 filenames = [os.path.join(directory, fn) for fn in next(os.walk(directory))[2]] # list files, no directories, full path
-for a in filenames:
+for a in filenames_no_dirs:
     filename1 = a
+    filename2 = a
     if options[1] in sys.argv:
         try:
             prepend = (sys.argv[(sys.argv.index(options[1])) + 1])
@@ -29,8 +29,8 @@ for a in filenames:
             append = (sys.argv[(sys.argv.index(options[2])) + 1])
             if append in options:
                 sys.exit("Invalid Prepend.")
-            filename1 = filename1 + append
+            filename1 = os.path.splitext(filename1)[0] + append + os.path.splitext(filename1)[1]
         except IndexError:
             sys.exit("Invalid Append.")
-    final_name.append(filename1)
+    final_name.append(directory + filename1)
 print(final_name)
