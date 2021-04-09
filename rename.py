043ab11e27a07e4bb85aa448cd7e5caa36a4a0ef
Vernon Wenberg <vernon@wenberg.net>
2021-04-09 13:07:47 -0500

    Implement file renaming.

--- rename.py
+++ rename.py
@@ -2,35 +2,42 @@ import os, sys
 
 options = ("-d", "--prepend", "--append")
 
-if len(sys.argv) < 4:
-    sys.exit("Nothing to do.")
-elif options[0] not in sys.argv:
-    sys.exit("No directory specified.")
-final_name = []
+def main():
+    if len(sys.argv) < 4:
+        sys.exit("Nothing to do.")
+    elif options[0] not in sys.argv:
+        sys.exit("No directory specified.")
+    directory = (sys.argv[(sys.argv.index(options[0])) + 1])
+    if not os.path.isdir(directory):
+        sys.exit("Specified directory is not a valid directory.")
+    do_dir(directory)
 
-directory = (sys.argv[(sys.argv.index(options[0])) + 1])
-if not os.path.isdir(directory):
-    sys.exit("Specified directory is not a valid directory.")
-filenames_no_dirs = next(os.walk(directory))[2] # list of files, just filenames, no directories 
-filenames = [os.path.join(directory, fn) for fn in next(os.walk(directory))[2]] # list files, no directories, full path
-for a in filenames_no_dirs:
-    filename1 = a
-    filename2 = a
-    if options[1] in sys.argv:
-        try:
-            prepend = (sys.argv[(sys.argv.index(options[1])) + 1])
-            if prepend in options:
+def do_dir(directory):
+    final_name = []
+    filenames_no_dirs = next(os.walk(directory))[2] # list of files, just filenames, no directories 
+    filenames = [os.path.join(directory, fn) for fn in next(os.walk(directory))[2]] # list files, no directories, full path
+    for a in filenames_no_dirs:
+        filename1 = a
+        if options[1] in sys.argv:
+            try:
+                prepend = (sys.argv[(sys.argv.index(options[1])) + 1])
+                if prepend in options:
+                    sys.exit("Invalid Prepend.")
+                filename1 = prepend + filename1
+            except IndexError:
                 sys.exit("Invalid Prepend.")
-            filename1 = prepend + filename1
-        except IndexError:
-            sys.exit("Invalid Prepend.")
-    if options[2] in sys.argv:
-        try:
-            append = (sys.argv[(sys.argv.index(options[2])) + 1])
-            if append in options:
-                sys.exit("Invalid Prepend.")
-            filename1 = os.path.splitext(filename1)[0] + append + os.path.splitext(filename1)[1]
-        except IndexError:
-            sys.exit("Invalid Append.")
-    final_name.append(directory + filename1)
-print(final_name)
+        if options[2] in sys.argv:
+            try:
+                append = (sys.argv[(sys.argv.index(options[2])) + 1])
+                if append in options:
+                    sys.exit("Invalid Prepend.")
+                filename1 = os.path.splitext(filename1)[0] + append + os.path.splitext(filename1)[1]
+            except IndexError:
+                sys.exit("Invalid Append.")
+        print("{} will be renamed to {}".format(directory + a, directory + filename1))
+        os.rename(directory + a, directory + filename1)
+        final_name.append(directory + filename1)
+    return(final_name)
+
+if __name__ == "__main__":
+    main()   
