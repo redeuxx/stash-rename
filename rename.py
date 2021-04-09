bbf848413b84c186183bacafad8c32ac89e5c403
Vernon Wenberg <vernon@wenberg.net>
2021-04-09 05:18:51 -0500

    Validate inputs.

--- rename.py
+++ rename.py
@@ -1,6 +1,7 @@
 import os, sys
 
 options = ("-d", "--prepend", "--append")
+print(options)
 
 if len(sys.argv) < 4:
     sys.exit("Nothing to do.")
@@ -9,21 +10,25 @@ elif options[0] not in sys.argv:
 final_name = []
 
 directory = (sys.argv[(sys.argv.index(options[0])) + 1])
+if not os.path.isdir(directory):
+    sys.exit("Specified directory is not a valid directory.")
 filenames_dirs = next(os.walk(directory))[2] # list of files, just filenames, no directories 
 filenames = [os.path.join(directory, fn) for fn in next(os.walk(directory))[2]] # list files, no directories, full path
-print(options[2])
 for a in filenames:
     filename1 = a
     if options[1] in sys.argv:
         try:
             prepend = (sys.argv[(sys.argv.index(options[1])) + 1])
-            for a in filenames:
-                filename1 = prepend + filename1
+            if prepend in options:
+                sys.exit("Invalid Prepend.")
+            filename1 = prepend + filename1
         except IndexError:
             sys.exit("Invalid Prepend.")
     if options[2] in sys.argv:
         try:
             append = (sys.argv[(sys.argv.index(options[2])) + 1])
+            if append in options:
+                sys.exit("Invalid Prepend.")
             filename1 = filename1 + append
         except IndexError:
             sys.exit("Invalid Append.")
