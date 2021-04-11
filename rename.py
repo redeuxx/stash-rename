aec4fe599af4008af972f3b458de44ff926607a8
Vernon Wenberg <vernon@wenberg.net>
2021-04-11 09:27:37 -0500

    Pass raw string to do_dir().

--- rename.py
+++ rename.py
@@ -10,12 +10,13 @@ def main():
     directory = (sys.argv[(sys.argv.index(options[0])) + 1])
     if not os.path.isdir(directory):
         sys.exit("Specified directory is not a valid directory.")
-    do_dir(directory)
+    do_dir(r"%s" % directory)
 
 def do_dir(directory):
+    """Main function to do file operations. Pass the directory from command line arguments, 
+    --append, --prepend. Does not return anything."""
     final_name = []
     filenames_no_dirs = next(os.walk(directory))[2] # list of files, just filenames, no directories 
-    filenames = [os.path.join(directory, fn) for fn in next(os.walk(directory))[2]] # list files, no directories, full path
     for a in filenames_no_dirs:
         filename1 = a
         if options[1] in sys.argv:
@@ -37,7 +38,6 @@ def do_dir(directory):
         print("{} will be renamed to {}".format(directory + a, directory + filename1))
         os.rename(directory + a, directory + filename1)
         final_name.append(directory + filename1)
-    return(final_name)
 
 if __name__ == "__main__":
     main()   
