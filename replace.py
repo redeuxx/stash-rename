40afe19abb0b31d3675812d1d31419f59870580a
Vernon Wenberg <vernon@wenberg.net>
2021-04-14 12:38:06 -0500

    Implement renaming and absolute paths.

--- replace.py
+++ replace.py
@@ -12,9 +12,9 @@ def main():
             sys.exit("No database file specified.")
     except IndexError:
         syntax_error()
-
-    print(f"Processing {directory}")
     filenames_no_dirs = next(os.walk(directory))[2]
+    absolute_path = os.path.abspath(directory)
+    print(f"Processing {absolute_path}")
     with open(database_file, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         num_count = 0
@@ -22,9 +22,10 @@ def main():
             for x in filenames_no_dirs:
                 if re.match(row["abbreviation"], x):
                     w = re.match(row["abbreviation"], x)
-                    new_name = x.replace(w.group(), row['full_name'])
-                    num_count += 1
+                    new_name = absolute_path + "/" + x.replace(w.group(), row['full_name'])
+                    os.rename(absolute_path + "/" + x, new_name)
                     print(f"{x} will renamed to {new_name}")
+                    num_count += 1
     print(f"Renamed {num_count} files.")
 
 def syntax_error():
