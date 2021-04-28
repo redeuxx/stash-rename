5a333a3e9a7e675b222072731a87f47516dfcfb4
Vernon Wenberg <vernon@wenberg.net>
2021-04-28 02:51:22 -0500

    add second argument to remove characters from eof

--- functions.py
+++ functions.py
@@ -2,9 +2,6 @@
 
 import os, sys, re
 
-def syntax_error():
-    '''TODO'''
-
 def list_filenames(directory):
     filenames = [os.path.join(directory, fn) for fn in next(os.walk(directory))[2]]
     return filenames
@@ -12,9 +9,10 @@ def list_filenames(directory):
 def list_directories(directory):
     return [f.path for f in os.scandir(directory) if f.is_dir()]
 
-def print_hello():
-    print("Hello World")
-
-def move_biggest(directory):
-    '''TODO'''
-    print(directory)
+def match_all_after(string_to_match, string_to_remove):
+    try:
+        a = string_to_match[string_to_match.index(string_to_remove):]
+        b = string_to_match.replace(a, "")
+        return b.lower()
+    except ValueError:
+        return 0
--- moveb.py
+++ moveb.py
@@ -9,6 +9,11 @@ def main():
             sys.exit("Invalid directory.")
     except IndexError:
         sys.exit("No directory specified.")
+    try:
+        if len(sys.argv[2]) > 0:
+            remove_this_string = sys.argv[2]
+    except IndexError:
+        remove_this_string = ""
     directory = os.path.abspath(r"%s" % sys.argv[1])
     directory_listing = functions.list_directories(directory)
     for p in directory_listing:
@@ -25,14 +30,29 @@ def main():
         if max_size == 0:
             print("All files were 0 in size or there were no files in %s" % (p))
         else:
-            move_biggest(os.path.abspath(max_filename), directory)
+            move_biggest(os.path.abspath(max_filename), directory, remove_this_string)
 
-def move_biggest(filename, directory):
+def move_biggest(filename, directory, remove_this_string):
     previous_fullpath = pathlib.PurePath(filename) # full path to file
-    a = previous_fullpath.parts[-2] + pathlib.Path(previous_fullpath.parts[-1]).suffix # create new filename
-    new_filename= os.path.join(directory, a) # create new full path
-    print("%s will be moved to %s" % (previous_fullpath, new_filename))
-    os.rename(previous_fullpath, new_filename)
+    previous_filename = os.fspath(pathlib.Path(previous_fullpath.parts[-1])) # convert path object to string
+    suffix = pathlib.Path(previous_fullpath.parts[-1]).suffix
+
+    """If second argument is specified, remove specified string from new filename"""
+    if len(remove_this_string) > 0:
+        print("here1")
+        if functions.match_all_after(previous_filename, remove_this_string) == 0:
+            print("Substring not found.")
+        else:
+            fixed_string = functions.match_all_after(previous_filename, remove_this_string)
+            new_filename= os.path.join(directory, fixed_string + suffix) # create new full path
+            print("%s will be moved to %s" % (previous_fullpath, new_filename))
+            os.rename(previous_fullpath, new_filename)
+    else:
+        new_filename= os.path.join(directory, previous_filename + suffix) # create new full path
+        print("%s will be moved to %s" % (previous_fullpath, new_filename))
+        print("here")
+        os.rename(previous_fullpath, new_filename)
+        
 
 if __name__ == "__main__":
     main()
